a82f783c800dc3508d24cebdbc091607a9298ed5
toall1985 <t_allen@hotmail.co.uk>
2016-08-06 20:05:57 +0100

    Football update

--- plugin.video.footballrepeat/default.py
+++ plugin.video.footballrepeat/default.py
@@ -18,6 +18,7 @@ import sys
 import urlparse
 import urllib,urllib2,datetime,re,os,base64,xbmc,xbmcplugin,xbmcgui,xbmcaddon,xbmcvfs,traceback,cookielib,urlparse,httplib,time
 import urlresolver
+import yt
 
 Dialog = xbmcgui.Dialog()
 Decode = base64.decodestring
@@ -43,6 +44,76 @@ FANART      =  xbmc.translatePath(os.path.join(ADDONS,addon_id,'fanart.jpg'))
 def Home_Menu():
     addDirFolder('Highlights','',3,ART + 'icon.png',FANART,'')
     addDirFolder('Fixtures','',4,ART + 'icon.png',FANART,'')
+    addDirFolder('Premier League Table','http://www.sportinglife.com/football/premier-league/table',6,ART + 'icon.png',FANART,'')
+
+def Prem_Table(url):	
+    addDirFolder('[COLORwhite]                                                    pl        w        d        l        f        a        pts[/COLOR]','','','','','')
+    html=OPEN_URL(url)
+    match = re.compile('<td>(.+?)</td>.+?<td class="ixt div"><strong><a href="([^"]*)">([^>]*)</a></strong></td>.+?<td class="div">([^>]*)</td>.+?<td>([^>]*)</td>.+?<td>([^>]*)</td>.+?<td>([^>]*)</td>.+?<td class="mobile-hdn">([^>]*)</td>.+?<td class="mobile-hdn">([^>]*)</td>.+?<td class="div">([^>]*)</td>.+?<td class="div">([^>]*)</td>',re.DOTALL).findall(html)
+    for pos,url,team,pl,w,d,l,f,a,pts,dif in match:
+        team = team
+        if 'Arsenal' in team:
+            image = 'http://s018.radikal.ru/i519/1210/74/a0965770c1bd.png'
+            name = '[COLORwhite]'+pos+' - '+team+'[/COLOR]                                  '+pl+'        '+w+'        '+d+'        '+l+'        '+f+'        '+a+'        '+pts
+        elif 'Bournemouth' in team:
+            image = 'http://soccerlogo.net/uploads/posts/2015-02/1424200737_fc-afc-bournemouth.png'
+            name = '[COLORwhite]'+pos+' - '+team+'[/COLOR]                       '+pl+'        '+w+'        '+d+'        '+l+'        '+f+'        '+a+'        '+pts
+        elif 'Burnley' in team:
+            image = 'https://www.google.co.uk/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=0ahUKEwiOl4Xrna3OAhVqDcAKHYLQCWoQjRwIBw&url=http%3A%2F%2Fwww.sinfuliphone.com%2Fshowpost.php%3Fp%3D590636%26postcount%3D10&bvm=bv.129391328,d.ZGg&psig=AFQjCNFD5vNL2PaGAi-RtJbAYMcfGrGhiw&ust=1470588123447720'
+            name = '[COLORwhite]'+pos+' - '+team+'[/COLOR]                                   '+pl+'        '+w+'        '+d+'        '+l+'        '+f+'        '+a+'        '+pts
+        elif 'Chelsea' in team:
+            image = 'http://soccerlogo.net/uploads/posts/2014-09/1410462243_fc-chelsea.png'
+            name = '[COLORwhite]'+pos+' - '+team+'[/COLOR]                                  '+pl+'        '+w+'        '+d+'        '+l+'        '+f+'        '+a+'        '+pts
+        elif 'Crystal' in team:
+            image = 'http://jonwant.com/wp-content/uploads/2015/04/crystalpalace.png'
+            name = '[COLORwhite]'+pos+' - '+team+'[/COLOR]                       '+pl+'        '+w+'        '+d+'        '+l+'        '+f+'        '+a+'        '+pts
+        elif 'Everton' in team:
+            image = 'http://megaicons.net/static/img/icons_sizes/257/622/256/everton-fc-icon.png'
+            name = '[COLORwhite]'+pos+' - '+team+'[/COLOR]                                   '+pl+'        '+w+'        '+d+'        '+l+'        '+f+'        '+a+'        '+pts
+        elif 'Hull' in team:
+            image = 'http://www.fm-base.co.uk/forum/attachments/football-manager-2013-manager-stories/367359d1373707600-molineux-theatre-dreams-wolves-story-hull.png'
+            name = '[COLORwhite]'+pos+' - '+team+'[/COLOR]                                 '+pl+'        '+w+'        '+d+'        '+l+'        '+f+'        '+a+'        '+pts
+        elif 'Leicester' in team:
+            image = 'http://soccerlogo.net/uploads/posts/2014-09/1410463960_fc-leicester-city.png'
+            name = '[COLORwhite]'+pos+' - '+team+'[/COLOR]                       '+pl+'        '+w+'        '+d+'        '+l+'        '+f+'        '+a+'        '+pts
+        elif 'Liverpool' in team:
+            image = 'http://i641.photobucket.com/albums/uu140/marveljoe_bucket/Liverpool-FC-256x256.png'
+            name = '[COLORwhite]'+pos+' - '+team+'[/COLOR]                               '+pl+'        '+w+'        '+d+'        '+l+'        '+f+'        '+a+'        '+pts
+        elif 'Manchester City' in team:
+            image = 'http://icons.iconseeker.com/png/fullsize/british-football-club/manchester-city.png'
+            name = '[COLORwhite]'+pos+' - '+team+'[/COLOR]               '+pl+'        '+w+'        '+d+'        '+l+'        '+f+'        '+a+'        '+pts
+        elif 'Manchester United' in team:
+            image = 'https://hdlogo.files.wordpress.com/2013/11/manchester-united.png'
+            name = '[COLORwhite]'+pos+' - '+team+'[/COLOR]          '+pl+'        '+w+'        '+d+'        '+l+'        '+f+'        '+a+'        '+pts
+        elif 'Middlesbrough' in team:
+            image = 'http://s25.postimg.org/g611tr767/Badge_Middlesbrough256x256.png'
+            name = '[COLORwhite]'+pos+' - '+team+'[/COLOR]                 '+pl+'        '+w+'        '+d+'        '+l+'        '+f+'        '+a+'        '+pts
+        elif 'Southampton' in team:
+            image = 'http://s019.radikal.ru/i639/1210/48/3326d080e375.png'
+            name = '[COLORwhite]'+pos+' - '+team+'[/COLOR]                   '+pl+'        '+w+'        '+d+'        '+l+'        '+f+'        '+a+'        '+pts
+        elif 'Stoke City' in team:
+            image = 'http://s55.radikal.ru/i147/1210/96/e3f610ab745c.png'
+            name = '[COLORwhite]'+pos+' - '+team+'[/COLOR]                          '+pl+'        '+w+'        '+d+'        '+l+'        '+f+'        '+a+'        '+pts
+        elif 'Sunderland' in team:
+            image = 'http://futhead.cursecdn.com/static/img/16/clubs/106.png'
+            name = '[COLORwhite]'+pos+' - '+team+'[/COLOR]                        '+pl+'        '+w+'        '+d+'        '+l+'        '+f+'        '+a+'        '+pts
+        elif 'Swansea' in team:
+            image = 'http://soccerlogo.net/uploads/posts/2014-09/1410462864_fc-swansea_city.png'
+            name = '[COLORwhite]'+pos+' - '+team+'[/COLOR]                    '+pl+'        '+w+'        '+d+'        '+l+'        '+f+'        '+a+'        '+pts
+        elif 'Tottenham' in team:
+            image = 'http://s14.radikal.ru/i187/1210/d2/243ffe6f2f90.png'
+            name = '[COLORwhite]'+pos+' - '+team+'[/COLOR]        '+pl+'        '+w+'        '+d+'        '+l+'        '+f+'        '+a+'        '+pts
+        elif 'Watford' in team:
+            image = 'http://s25.postimg.org/bclw2n027/Badge_Watford256x256.png'
+            name = '[COLORwhite]'+pos+' - '+team+'[/COLOR]                              '+pl+'        '+w+'        '+d+'        '+l+'        '+f+'        '+a+'        '+pts
+        elif 'Bromwich' in team:
+            image = 'http://s018.radikal.ru/i516/1210/6c/d0990201b8d2.png'
+            name = '[COLORwhite]'+pos+' - '+team+'[/COLOR]   '+pl+'        '+w+'        '+d+'        '+l+'        '+f+'        '+a+'        '+pts
+        elif 'West Ham' in team:
+            image = 'http://s018.radikal.ru/i502/1210/60/c38b78fbbdb1.png'
+            name = '[COLORwhite]'+pos+' - '+team+'[/COLOR]             '+pl+'        '+w+'        '+d+'        '+l+'        '+f+'        '+a+'        '+pts
+        addDirFolder(str(name),(Decode('aHR0cDovL3d3dy5mdWxsbWF0Y2hlc2FuZHNob3dzLmNvbS8/cz0=')+team).replace(' ','+'),1,image,image,'')
+    
 
 def FootballFixturesDay():
     html=OPEN_URL(Decode('aHR0cDovL2xpdmVvbnNhdC5jb20vcXVpY2tpbmRleC5odG1s'))
@@ -68,34 +139,69 @@ def FootballFixturesGame(url):
 
 def Football_Highlights():
 
-    addDirFolder('Latest','http://www.fullmatchesandshows.com',1,'http://www.fancyicons.com/free-icons/125/miscellaneous/png/256/football_256.png',ART + 'fanart.jpg','')
-    addDirFolder('EURO 2016','http://www.fullmatchesandshows.com/euro-2016/',1,'http://football.mywapblog.com/files/uefa-euro-2016-logo.png',ART + 'fanart.jpg','')
-    addDirFolder('Shows','http://www.fullmatchesandshows.com/category/show/',1,'http://www.fm-base.co.uk/forum/attachments/club-competition-logos/3885-soccer-am-logo-socceram.png',ART + 'fanart.jpg','')
-    addDirFolder('Premier League','http://www.fullmatchesandshows.com/premier-league/',1,'https://footballseasons.files.wordpress.com/2013/05/premier-league.png',ART + 'fanart.jpg','')
-    addDirFolder('La Liga','http://www.fullmatchesandshows.com/la-liga/',1,'http://1.bp.blogspot.com/-c6kQ40ryhyo/U19cUlz25sI/AAAAAAAABak/qtn5chSFZm0/s1600/la-liga-logo_display_image.png',ART + 'fanart.jpg','')
-    addDirFolder('Bundesliga','http://www.fullmatchesandshows.com/bundesliga/',1,'http://m.img.brothersoft.com/iphone/189/518670189_icon175x175.jpg',ART + 'fanart.jpg','')
-    addDirFolder('Champions League','http://www.fullmatchesandshows.com/champions-league/',1,'http://www.ecursuri.ro/images/teste/test-champions-league.jpg',ART + 'fanart.jpg','')
-    addDirFolder('Serie A','http://www.fullmatchesandshows.com/category/serie-a/',1,'http://files.jcriccione.it/200000223-2484526782/serie%20a.png',ART + 'fanart.jpg','')
-    addDirFolder('Ligue 1','http://www.fullmatchesandshows.com/category/ligue-1/',1,'http://a1.mzstatic.com/us/r30/Purple5/v4/37/c7/44/37c744ae-5824-42b7-6ce0-5f471f52baab/icon180x180.jpeg',ART + 'fanart.jpg','')
-    addDirFolder('Copa America 2015','http://www.fullmatchesandshows.com/copa-america-2015/',1,'https://pbs.twimg.com/profile_images/521966985907691520/Nq9OAPIo_400x400.png',ART + 'fanart.jpg','')
-    addDirFolder('CONCACAF','http://www.fullmatchesandshows.com/category/concacaf/',1,'http://a3.mzstatic.com/us/r30/Purple3/v4/40/26/14/4026147c-7022-4ca3-504e-e78950cc3f1c/icon175x175.png',ART + 'fanart.jpg','')
-    addDirFolder('Women World Cup','http://www.fullmatchesandshows.com/category/women-world-cup/',1,'https://upload.wikimedia.org/wikipedia/en/thumb/7/76/2015_FIFA_Women\'s_World_Cup_logo.svg/967px-2015_FIFA_Women\'s_World_Cup_logo.svg.png',ART + 'fanart.jpg','' )
+    addDirFolder('Latest','http://www.fullmatchesandshows.com',8,'http://www.fancyicons.com/free-icons/125/miscellaneous/png/256/football_256.png',FANART,'')
+    addDirFolder('EURO 2016','http://www.fullmatchesandshows.com/euro-2016/',8,'http://football.mywapblog.com/files/uefa-euro-2016-logo.png',FANART,'')
+    addDirFolder('Shows','http://www.fullmatchesandshows.com/category/show/',8,'http://www.fm-base.co.uk/forum/attachments/club-competition-logos/3885-soccer-am-logo-socceram.png',FANART,'')
+    addDirFolder('Premier League','http://www.fullmatchesandshows.com/premier-league/',8,'https://footballseasons.files.wordpress.com/2013/05/premier-league.png',FANART,'')
+    addDirFolder('La Liga','http://www.fullmatchesandshows.com/la-liga/',8,'http://1.bp.blogspot.com/-c6kQ40ryhyo/U19cUlz25sI/AAAAAAAABak/qtn5chSFZm0/s1600/la-liga-logo_display_image.png',FANART,'')
+    addDirFolder('Bundesliga','http://www.fullmatchesandshows.com/bundesliga/',8,'http://m.img.brothersoft.com/iphone/189/518670189_icon175x175.jpg',FANART,'')
+    addDirFolder('Champions League','http://www.fullmatchesandshows.com/champions-league/',8,'http://www.ecursuri.ro/images/teste/test-champions-league.jpg',FANART,'')
+    addDirFolder('Serie A','http://www.fullmatchesandshows.com/category/serie-a/',8,'http://files.jcriccione.it/200000223-2484526782/serie%20a.png',FANART,'')
+    addDirFolder('Ligue 1','http://www.fullmatchesandshows.com/category/ligue-1/',8,'http://a1.mzstatic.com/us/r30/Purple5/v4/37/c7/44/37c744ae-5824-42b7-6ce0-5f471f52baab/icon180x180.jpeg',FANART,'')
+    addDirFolder('Copa America 2015','http://www.fullmatchesandshows.com/copa-america-2015/',8,'https://pbs.twimg.com/profile_images/521966985907691520/Nq9OAPIo_400x400.png',FANART,'')
+    addDirFolder('CONCACAF','http://www.fullmatchesandshows.com/category/concacaf/',8,'http://a3.mzstatic.com/us/r30/Purple3/v4/40/26/14/4026147c-7022-4ca3-504e-e78950cc3f1c/icon175x175.png',FANART,'')
+    addDirFolder('Women World Cup','http://www.fullmatchesandshows.com/category/women-world-cup/',8,'https://upload.wikimedia.org/wikipedia/en/thumb/7/76/2015_FIFA_Women\'s_World_Cup_logo.svg/967px-2015_FIFA_Women\'s_World_Cup_logo.svg.png',FANART,'' )
 
 
-		
-def get_All_Rows(url):
+def Get_the_rows(url,iconimage):
     HTML = OPEN_URL(url)
-    match = re.compile('class="entry-thumb" src="(.+?)" alt="".+?<h3 class="entry-title td-module-title"><a href="(.+?)" rel="bookmark" title=".+?">(.+?)</a></h3>',re.DOTALL).findall(HTML)
-    for img,url,name in match:
-        Name = name.replace('&#8211;', '-').replace('&#038;', '&')
-        addDir(Name,url,2,img,'','')
+    match2 = re.compile('<div class="td-module-thumb"><a href="(.+?)".+?title="(.+?)".+?"entry-thumb" src="(.+?)"').findall(HTML)
+    for url,name,img in match2:
+        if 'Full Match' in name:
+    	    Name = name.replace('&#8211;', '-').replace('&#038;', '&').replace('&#8217;', '')
+            addDirFolder(Name,url,7,img,'','')
+        else:
+    	    Name = name.replace('&#8211;', '-').replace('&#038;', '&').replace('&#8217;', '')
+            addDir(Name,url,2,img,'','')
+    Next = re.compile('<span class="current">.+?</span><a href="(.+?)" class="page" title=".+?">(.+?)</a>').findall(HTML)
+    for url,name in Next:
+        addDirFolder('NEXT PAGE',url,1,iconimage,FANART,'')
+		
+def get_All_Rows(url,iconimage):
+    HTML = OPEN_URL(url)
+    block = re.compile('<div class="td-block-span6">(.+?)<div class="td-pb-span4 td-main-sidebar">',re.DOTALL).findall(HTML)
+    match2 = re.compile('<div class="td-module-thumb"><a href="(.+?)".+?title="(.+?)".+?"entry-thumb" src="(.+?)"').findall(str(block))
+    for url,name,img in match2:
+        if 'Full Match' in name:
+    	    Name = name.replace('&#8211;', '-').replace('&#038;', '&').replace('&#8217;', '')
+            addDirFolder(Name,url,7,img,'','')
+        else:
+    	    Name = name.replace('&#8211;', '-').replace('&#038;', '&').replace('&#8217;', '')
+            addDir(Name,url,2,img,'','')
+    Next = re.compile('<span class="current">.+?</span><a href="(.+?)" class="page" title=".+?">(.+?)</a>').findall(HTML)
+    for url,name in Next:
+        addDirFolder('NEXT PAGE',url,1,iconimage,FANART,'')
 
+def get_Multi_Links(url,iconimage):
+    addDir('Extended Highlights',url,2,iconimage,FANART,'')
+    HTML = OPEN_URL(url)
+    match = re.compile('<link href=".+?" rel="stylesheet" type="text/css"><li tabindex="0" class="button_style" id=".+?"><a href="(.+?)"><div class="acp_title">(.+?)</div></a></li>').findall(HTML)
+    for url,name in match:
+        name = (name).replace('HL English','English Highlights')
+        addDir(name,url,2,iconimage,FANART,'')
+		
 def get_PLAYlink(url):
     HTML = OPEN_URL(url)
+    match_youtube = re.compile('<iframe width="560" height="315" src="https://www.youtube.com/embed/(.+?)" frameborder="0" allowfullscreen>').findall(HTML)
+    for url in match_youtube:
+        yt.PlayVideo(url)
     match = re.compile('<script data-config="(.+?)" data-height').findall(HTML)
     for playlink in match:
-        Playlink = (playlink).replace('/v2', '').replace('zeus.json', 'video-sd.mp4?hosting_id=21772').replace('config.playwire.com', 'cdn.video.playwire.com')
-        Resolve('http:'+Playlink)
+        if 'div' in playlink:
+            pass
+        else:
+            Playlink = (playlink).replace('/v2', '').replace('zeus.json', 'video-sd.mp4?hosting_id=21772').replace('config.playwire.com', 'cdn.video.playwire.com')
+            Resolve('http:'+Playlink)
 
 	
 
@@ -212,10 +318,13 @@ def setView(content, viewType):
 
 
 if mode == None     : Home_Menu()
-elif mode == 1 		: get_All_Rows(url)
+elif mode == 1 		: get_All_Rows(url,iconimage)
 elif mode == 2 		: get_PLAYlink(url)
 elif mode == 3 		: Football_Highlights()
 elif mode == 4 		: FootballFixturesDay()
 elif mode == 5 		: FootballFixturesGame(url)
+elif mode == 6 	 	: Prem_Table(url)
+elif mode == 7 		: get_Multi_Links(url,iconimage)
+elif mode == 8 		: Get_the_rows(url,iconimage)
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
