f2206d24fd3665e15588822aca724c92f4863b4f
toall1985 <t_allen@hotmail.co.uk>
2017-08-08 18:36:32 +0100

    1080p

---
+++ plugin.video.1080pmovie/default.py
@@ -0,0 +1,291 @@
+# -*- coding: utf-8 -*-
+
+import requests, xbmcgui, xbmcplugin, xbmc, re, sys, os, xbmcaddon, json, urllib
+from threading import Thread
+ADDON_PATH = xbmc.translatePath('special://home/addons/plugin.video.1080pmovie/')
+ICON = ADDON_PATH + 'icon.png'
+FANART = ADDON_PATH + 'fanart.jpg'
+USERDATA_PATH = xbmc.translatePath('special://home/userdata/addon_data')
+ADDON_DATA = USERDATA_PATH + '/plugin.video.1080pmovie/'
+favourites = ADDON_DATA + 'favourites'
+if os.path.exists(favourites) == True:
+   FAV = open(favourites).read()
+else:
+   FAV = []
+
+def Main_Menu():
+   Menu('Action & Adventure','https://1080pmovie.com/action-adventure/',1,ICON,FANART,'','')
+   Menu('Animation','https://1080pmovie.com/animation/',1,ICON,FANART,'','')
+   Menu('Biography','https://1080pmovie.com/biography/',1,ICON,FANART,'','')
+   Menu('Comedy','https://1080pmovie.com/comedy/',1,ICON,FANART,'','')
+   Menu('Crime','https://1080pmovie.com/crime/',1,ICON,FANART,'','')
+   Menu('Documentary','https://1080pmovie.com/documentary/',1,ICON,FANART,'','')
+   Menu('Drama','https://1080pmovie.com/drama/',1,ICON,FANART,'','')
+   Menu('Family','https://1080pmovie.com/family/',1,ICON,FANART,'','')
+   Menu('Fantasy','https://1080pmovie.com/fantasy/',1,ICON,FANART,'','')
+   Menu('History','https://1080pmovie.com/history/',1,ICON,FANART,'','')
+   Menu('Horror','https://1080pmovie.com/horror/',1,ICON,FANART,'','')
+   Menu('Musical','https://1080pmovie.com/musical/',1,ICON,FANART,'','')
+   Menu('Mystery','https://1080pmovie.com/mystery/',1,ICON,FANART,'','')
+   Menu('Romance','https://1080pmovie.com/romance/',1,ICON,FANART,'','')
+   Menu('Sci-Fi','https://1080pmovie.com/sci-fi/',1,ICON,FANART,'','')
+   Menu('Sport','https://1080pmovie.com/sport/',1,ICON,FANART,'','')
+   Menu('Thriller','https://1080pmovie.com/thriller/',1,ICON,FANART,'','')
+   Menu('War','https://1080pmovie.com/war/',1,ICON,FANART,'','')
+   Menu('Western','https://1080pmovie.com/western/',1,ICON,FANART,'','')
+   Menu('Search','',2,ICON,FANART,'','')
+
+def get_info(url):
+	html = requests.get(url).content
+	match = re.compile('<a class="video-item".+?href="(.+?)".+?title="(.+?)".+?<img.+?src="(.+?)"').findall(html)
+	for url,name,img in match:
+		name = name.replace('Watch','').replace('#038;','').replace('&#8211;','-')
+		Play(name,url,3,img,FANART,'','')
+	next = re.compile('link rel="next" href="(.+?)"').findall(html)
+	for n in next:
+		Menu('Next Page',n,1,ICON,FANART,'','')
+		
+def get_playlink(url):
+	html = requests.get(url).content
+	match = re.compile('<iframe src="(.+?)"').findall(html)
+	for m in match:
+		html2 = requests.get(m).content
+		match2 = re.compile('iframe src="(.+?)"').findall(html2)
+		for playlink in match2:
+			resolve(playlink)
+	
+   
+def Search():
+   Search_url = 'https://1080pmovie.com/?s= '
+   Dialog = xbmcgui.Dialog()
+   Search_title = Dialog.input('Search', type=xbmcgui.INPUT_ALPHANUM)
+   Search_name = Search_title.replace(' ','+20').lower()
+   url = Search_url+Search_name
+   get_info(url)
+	
+def setView(content, viewType):
+   # set content type so library shows more views and info
+   if content:
+      xbmcplugin.setContent(int(sys.argv[1]), content)
+   if ADDON.getSetting('auto-view')=='true':
+      xbmc.executebuiltin("Container.SetViewMode(%s)" % ADDON.getSetting(viewType) )
+		
+		
+def Menu(name,url,mode,iconimage,fanart,description,extra,showcontext=True,allinfo={}):
+      fav_mode = mode
+      u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&iconimage="+urllib.quote_plus(iconimage)+"&fanart="+urllib.quote_plus(fanart)+"&description="+urllib.quote_plus(description)+"&extra="+urllib.quote_plus(extra)
+      ok=True
+      liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+      liz.setInfo( type="Video", infoLabels={ "Title": name, "Plot": description } )
+      liz.setProperty( "Fanart_Image", fanart )
+      if showcontext:
+         contextMenu = []
+         if showcontext == 'fav':
+            contextMenu.append(('Remove from 1080pmovie Favorites','XBMC.RunPlugin(%s?mode=12&name=%s)'
+                           %(sys.argv[0], urllib.quote_plus(name))))
+         if not name in FAV:
+            contextMenu.append(('Add to 1080pmovie Favourites','XBMC.RunPlugin(%s?mode=11&name=%s&url=%s&iconimage=%s&fav_mode=%s&fanart=%s&description=%s)'
+                   %(sys.argv[0], urllib.quote_plus(name), urllib.quote_plus(url), urllib.quote_plus(iconimage), fav_mode, urllib.quote_plus(fanart), urllib.quote_plus(description))))
+         liz.addContextMenuItems(contextMenu)
+      ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+      return ok
+      xbmcplugin.endOfDirectory(int(sys.argv[1]))
+      
+
+		
+def Play(name,url,mode,iconimage,fanart,description,extra,showcontext=True,allinfo={}):
+      fav_mode = mode
+      u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&iconimage="+urllib.quote_plus(iconimage)+"&fanart="+urllib.quote_plus(fanart)+"&description="+urllib.quote_plus(description)+"&extra="+urllib.quote_plus(extra)
+      ok=True
+      liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+      liz.setInfo( type="Video", infoLabels={ "Title": name, "Plot": description } )
+      liz.setProperty( "Fanart_Image", fanart )
+      if showcontext:
+         contextMenu = []
+         if showcontext == 'fav':
+            contextMenu.append(('Remove from 1080pmovie Favorites','XBMC.RunPlugin(%s?mode=12&name=%s)'
+                           %(sys.argv[0], urllib.quote_plus(name))))
+         if not name in FAV:
+            contextMenu.append(('Add to 1080pmovie Favourites','XBMC.RunPlugin(%s?mode=11&name=%s&url=%s&iconimage=%s&fav_mode=%s&fanart=%s&description=%s)'
+                   %(sys.argv[0], urllib.quote_plus(name), urllib.quote_plus(url), urllib.quote_plus(iconimage), fav_mode, urllib.quote_plus(fanart), urllib.quote_plus(description))))
+         liz.addContextMenuItems(contextMenu)
+         contextMenu.append(('Queue Item', 'RunPlugin(%s?mode=14)' % sys.argv[0]))
+      ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
+      return ok
+      xbmcplugin.endOfDirectory(int(sys.argv[1]))
+		
+		
+# ===============================Favourites-----------Not sure whos code this is but credit due to them-------------------------------
+
+def addFavorite(name, url, mode, iconimage, fanart, description, extra):
+   favList = []
+   xbmc.log(extra)
+   try:
+      name = name.encode('utf-8', 'ignore')
+   except:
+      pass
+   if os.path.exists(favourites) == False:
+      favList.append((name, url, mode, iconimage, fanart, description, extra))
+      a = open(favourites, "w")
+      a.write(json.dumps(favList))
+      a.close()
+   else:
+      a = open(favourites).read()
+      data = json.loads(a)
+      data.append((name, url, mode, iconimage, fanart, description, extra))
+      b = open(favourites, "w")
+      b.write(json.dumps(data))
+      b.close()
+
+
+def getFavourites():
+   if not os.path.exists(favourites):
+      favList = []
+      favList.append(('1080pmovie Favourites Section', '', '', '', '', '', ''))
+      a = open(favourites, "w")
+      a.write(json.dumps(favList))
+      a.close()
+   else:
+      items = json.loads(open(favourites).read())
+      for i in items:
+         name = i[0]
+         url = i[1]
+         try:
+			   iconimage = i[3]
+         except:
+            iconimage = ''
+         try:
+            fanart = i[4]
+         except:
+            fanart = ''
+         try:
+            description = i[5]
+         except:
+            description = ''
+         try:
+            extra = i[6]
+         except:
+            extra = ''
+
+         if i[2] == 20:
+            Play(name, url, i[2], iconimage, fanart, description, extra, 'fav')
+         else:
+            Menu(name, url, i[2], iconimage, fanart, description, extra, 'fav')
+
+
+def rmFavorite(name):
+   data = json.loads(open(favourites).read())
+   for index in range(len(data)):
+      if data[index][0] == name:
+         del data[index]
+         b = open(favourites, "w")
+         b.write(json.dumps(data))
+         b.close()
+         break
+   xbmc.executebuiltin("XBMC.Container.Refresh")		
+
+def resolve(url): 
+	import urlresolver
+	try:
+		resolved_url = urlresolver.resolve(url)
+		xbmc.Player().play(resolved_url, xbmcgui.ListItem(name))
+	except:
+		xbmc.Player().play(url, xbmcgui.ListItem(name))
+	xbmcplugin.endOfDirectory(int(sys.argv[1]))
+	
+def get_params():
+      param=[]
+      paramstring=sys.argv[2]
+      if len(paramstring)>=2: 
+            params=sys.argv[2] 
+            cleanedparams=params.replace('?','')
+            if (params[len(params)-1]=='/'):
+                  params=params[0:len(params)-2]
+            pairsofparams=cleanedparams.split('&')
+            param={}   
+            for i in range(len(pairsofparams)):
+                  splitparams={}
+                  splitparams=pairsofparams[i].split('=')
+                  if (len(splitparams))==2:
+                        param[splitparams[0]]=splitparams[1]
+                        
+      return param
+      
+params=get_params()
+url=None
+name=None
+iconimage=None
+mode=None
+fanart=None
+description=None
+trailer=None
+fav_mode=None
+extra=None
+
+try:
+   extra=urllib.unquote_plus(params["extra"])
+except:
+   pass
+
+try:
+   fav_mode=int(params["fav_mode"])
+except:
+   pass
+
+try:
+      url=urllib.unquote_plus(params["url"])
+except:
+      pass
+try:
+      name=urllib.unquote_plus(params["name"])
+except:
+      pass
+try:
+      iconimage=urllib.unquote_plus(params["iconimage"])
+except:
+      pass
+try:      
+      mode=int(params["mode"])
+except:
+      pass
+try:      
+      fanart=urllib.unquote_plus(params["fanart"])
+except:
+      pass
+try:      
+      description=urllib.unquote_plus(params["description"])
+except:
+      pass
+
+#####################################################END PROCESSES##############################################################		
+		
+if mode == None: Main_Menu()
+elif mode == 1 : get_info(url)
+elif mode == 2 : Search()
+elif mode == 3 : get_playlink(url)
+
+elif mode == 10: getFavourites()
+elif mode==11:
+   try:
+      name = name.split('\\ ')[1]
+   except:
+      pass
+   try:
+      name = name.split('  - ')[0]
+   except:
+      pass
+   addFavorite(name, url, fav_mode, iconimage, fanart, description, extra)
+elif mode==12:
+   try:
+      name = name.split('\\ ')[1]
+   except:
+      pass
+   try:
+      name = name.split('  - ')[0]
+   except:
+      pass
+   rmFavorite(name)
+elif mode == 14 : queueItem()	
+elif mode == 20: resolve(url)
+
+xbmcplugin.endOfDirectory(int(sys.argv[1]))
