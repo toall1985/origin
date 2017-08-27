fbf79fd487813cb2da71a8e1ea511a2e561aebb0
toall1985 <t_allen@hotmail.co.uk>
2017-08-27 19:08:53 +0100

    retry

--- script.module.originresolver/lib/sources/template.py
+++ script.module.originresolver/lib/sources/template.py
@@ -4,11 +4,8 @@ import requests
 domain = ''
 name = ''
 sources = []
-url = ''
 
 def resolve(url):
     html = requests.get(url).content
-        sources.append({'source': name, 'quality': 'SD', 'scraper': name, 'url': playlink,'direct': True})
-    print sources
-
-resolve(url)
+        sources.append({'source': name, 'quality': 'HD', 'scraper': name, 'url': playlink,'direct': True})
+    return sources
