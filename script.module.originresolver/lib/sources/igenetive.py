a2edf40efd9c0da6d89f0f789d75906854b14c36
toall1985 <t_allen@hotmail.co.uk>
2017-08-27 19:15:28 +0100

    redo

---
+++ script.module.originresolver/lib/sources/igenetive.py
@@ -0,0 +1,14 @@
+import re
+import requests
+
+domain = 'igenetive.com'
+name = 'Igenetive'
+sources = []
+
+def resolve(url):
+    html = requests.get(url).content
+    playlink = re.findall('iframe src="(.+?)"')[0]
+        sources.append({'source': name, 'quality': 'SD', 'scraper': name, 'url': playlink,'direct': True})
+    print sources
+
+resolve(url)
